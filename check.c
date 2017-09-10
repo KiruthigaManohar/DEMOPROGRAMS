bd2ea28bfd11e7b82d4a561f27a75a7cbbed0a17
KiruthigaManohar <30401289+KiruthigaManohar@users.noreply.github.com>
2017-09-10 10:57:16 +0530

    Create check.c

---
+++ check.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char n;
+printf("Enter the character");
+scanf("%s",&n);
+if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u')
+printf("Its is vowel");
+else
+printf("consonant");
+}
