f3027a79cb0eaf8999be72ce07f98b1765526716
KiruthigaManohar <30401289+KiruthigaManohar@users.noreply.github.com>
2017-11-11 13:52:03 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,n,cal,i=1;
+printf(Enter the base);
+scanf("%d",&a);
+printf("Enter the exponent");
+scanf("%d",&n);
+if(i<=n)
+{
+cal=cal*a;
+}
+printf("The power of a number=%d",cal);
+}
