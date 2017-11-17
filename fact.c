a25277ef8143bc7d90fb66769b5216c3765046ce
KiruthigaManohar <30401289+KiruthigaManohar@users.noreply.github.com>
2017-11-17 20:05:49 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -1,12 +1,12 @@
-#incude<stdio.h>
+#include<stdio.h>
 void main()
 {
-int s=1,n;
+int s=1,a,n;
 printf("Enter the number:\t");
 scanf("%d",&n);
 for(a=1;a<=n;a++)
 {
 s=s*a;
 }
-printf("The factorial of a given number =\t");
+printf("The factorial of a given number =\t%d",s);
 }
