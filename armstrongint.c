5675c40a09488a524b3ed6ab8345aaa7697fe34f
KiruthigaManohar <30401289+KiruthigaManohar@users.noreply.github.com>
2017-11-20 15:03:21 +0530

    Create armstrongint.c

---
+++ armstrongint.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,r,sum,original,l;
+printf("Enter the number");
+scanf("%d",&n);
+printf("Enter the end point");
+scanf("%d",&l);
+for(i=n;i<=l;i++)
+{
+r=i%10;
+sum=sum+r*r*r;
+i=i/10;
+}
+if(sum==i)
+{
+printf("%d",i);
+}
+getch();
+}
