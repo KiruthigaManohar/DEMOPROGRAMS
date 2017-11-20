2021801fb66dd1140a456f4585830de709e20548
KiruthigaManohar <30401289+KiruthigaManohar@users.noreply.github.com>
2017-11-20 14:52:25 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,r,sum,original;
+printf("Enter the number");
+scanf("%d",&n);
+original=n;
+while(n!=0)
+{
+r=n%10;
+sum=sum+r*r*r;
+n=n/10;
+}
+if(original==sum)
+{
+printf("Armstrong number");
+else
+{
+printf("Not an armstrong");
+}
+getch();
+}
