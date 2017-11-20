52d3b6a5e2a2c1aebf4023d0978d44be2287cec5
KiruthigaManohar <30401289+KiruthigaManohar@users.noreply.github.com>
2017-11-20 15:04:39 +0530

    Update armstrongint.c

--- armstrongint.c
+++ armstrongint.c
@@ -2,7 +2,7 @@
 #include<conio.h>
 void main()
 {
-int n,r,sum,original,l;
+int n,r,sum,i,l;
 printf("Enter the number");
 scanf("%d",&n);
 printf("Enter the end point");
@@ -12,10 +12,10 @@ for(i=n;i<=l;i++)
 r=i%10;
 sum=sum+r*r*r;
 i=i/10;
-}
 if(sum==i)
 {
 printf("%d",i);
+}
 }
 getch();
 }
