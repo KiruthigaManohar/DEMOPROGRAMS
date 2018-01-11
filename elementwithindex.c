0bc491d3a5e5e34d5f08921f567b72fc53f41f42
KiruthigaManohar <30401289+KiruthigaManohar@users.noreply.github.com>
2018-01-11 16:45:23 +0530

    Create elementwithindex.c

---
+++ elementwithindex.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int n,a[n],i.sum=0,median;
+printf("Enter the Nth element");
+scanf("%d",&n);
+printf("Enter the array elements");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+printf("%d\t%d\n",a[i],i)
+}
+}
