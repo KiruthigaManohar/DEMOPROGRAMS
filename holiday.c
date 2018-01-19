f8ba221aa4b43f25a4d1bb0ad9a07b0937330307
KiruthigaManohar <30401289+KiruthigaManohar@users.noreply.github.com>
2018-01-19 08:39:38 +0000

    Create holiday.c

---
+++ holiday.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char str[20],i;
+printf("Enter the day");
+scanf("%c",&str);
+if(str=="saturday"||str=="sunday")
+{
+printf("Yes");
+}
+else
+{
+printf("No");
+}
+}
+
