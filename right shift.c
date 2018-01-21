d2090e6c3a6ee669c0af6e1c98be95dcc1d2cbf1
KiruthigaManohar <30401289+KiruthigaManohar@users.noreply.github.com>
2018-01-21 20:54:15 +0530

    Create right shift.c

---
+++ right shift.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int n[3],k,i;
+    printf("Enter the K number");
+    scanf("%d",&k);
+    printf("Enter the array elements");
+    for(i=0;i<10;i++)
+    {
+    scanf("%d",&n[i]);
+    }
+    for(i=0;i<10;i++)
+    {
+        n[i]>>k;
+        
+    }
+    printf("%d",n[i]);
+
+    
+}
