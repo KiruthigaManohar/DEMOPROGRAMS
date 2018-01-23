a9b2dcd1a2e316733306e933dbbf8f23f7ef275b
KiruthigaManohar <30401289+KiruthigaManohar@users.noreply.github.com>
2018-01-23 16:05:21 +0530

    Create check if number is present.c

---
+++ check if number is present.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,n;
+    printf("Enter the start and end point");
+    scanf("%d%d",&a,&b);
+    printf("Enter the number to check");
+    scanf("%d",&n);
+    if(n>=a&&n<=b)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("No");
+    }
+}
