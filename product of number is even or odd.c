5a4dfda3ef728b4ac12c5106b9172ef56a562859
KiruthigaManohar <30401289+KiruthigaManohar@users.noreply.github.com>
2018-01-24 21:25:33 +0530

    Create product of number is even or odd.c

---
+++ product of number is even or odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int m,n,a;
+    scanf("%d%d",&n,&m);
+    a=m*n;
+    if(a%2==0)
+    {
+        printf("Even");
+    }
+    else
+    {
+       printf("Odd");
+    
+    }
+}
