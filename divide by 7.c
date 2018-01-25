d6a732beb68c0714afc966bacbe7cedcb0535641
KiruthigaManohar <30401289+KiruthigaManohar@users.noreply.github.com>
2018-01-25 16:48:07 +0530

    Create divide by 7.c

---
+++ divide by 7.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int n,i,count=0;
+    printf("Enter the number");
+    scanf("%d",&n);
+        if(n%7==0)
+        {
+           printf("Yes");
+        }
+    
+    else
+    {
+        printf("No");
+    }
+}
