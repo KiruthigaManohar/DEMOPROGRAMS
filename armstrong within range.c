a71d8c7a03c3cfdad350bcde23660d9814e9ff29
KiruthigaManohar <30401289+KiruthigaManohar@users.noreply.github.com>
2018-02-04 19:51:49 +0530

    Update armstrong within range.c

--- armstrong within range.c
+++ armstrong within range.c
@@ -1,19 +1,25 @@
 #include<stdio.h>
 void main()
 {
-int a,b,r,sum,c,i;
-printf("Enter the range");
-scanf("%d%d",&a,&b);
-for(i=a;i<=b;i++)
-{sum =0;
-c=i;
-r=i%10;
-sum=sum+r*r*r;
-i=i/10;
-if(c==sum)
-{
-    printf("%d",sum);
-}
-}
+    int a,b,i,sum,c,r;
+    printf("Enter the range");
+    scanf("%d\t%d",&a,&b);
+    for(i=a;i<=b;i++)
+    {
+        sum=0;
+        c=i;
+    
+        while(c!=0)
+        {
+            r=c%10;
+            sum=sum+r*r*r;
+            c=c/10;
+        }
+    
+       if(i==sum)
+        {
+            printf("%d\n",i);
+        }
 
+    }
 }
