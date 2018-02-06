28acb3a88b31188ffb39cf9a9c1e94dedc1312ae
KiruthigaManohar <30401289+KiruthigaManohar@users.noreply.github.com>
2018-02-06 20:34:35 +0530

    Create odd num in numberN.c

---
+++ set8/odd num in numberN.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+void
+main ()
+{
+  int n,r;
+  scanf ("%d", &n);
+  while (n != 0)
+    {
+      r= n % 10;
+      if (r% 2 != 0)
+	{
+	  printf ("%d\t", r);
+	 
+	}
+	n=n/10;
+    }
+
+
+}
