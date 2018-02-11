c204eb88aa954441168f5d07db369aca88b34edb
KiruthigaManohar <30401289+KiruthigaManohar@users.noreply.github.com>
2018-02-11 09:08:35 +0000

    Create addition using pointers.c

---
+++ IBM-C Programs/addition using pointers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("Enter the values of a and b");
+    scanf("%d\t%d",&a,&b);
+    pointer(&a,&b);
+}
+pointer (int *c,int *d)
+{
+    *c=*c+*d;
+    printf("The addition value %d",*c);
+}
