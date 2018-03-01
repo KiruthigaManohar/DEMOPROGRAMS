95e54fad23355cfdb47e26b5d4a43ab21f76425b
KiruthigaManohar <30401289+KiruthigaManohar@users.noreply.github.com>
2018-03-01 15:55:49 +0530

    Create append.c

---
+++ Player/set1/append.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char s[100];
+    int l;
+    printf("Enter the string");
+    scanf("%s",s);
+    l=strlen(s);
+    s[l]='.';
+    printf("%s",s);
+    
+}
