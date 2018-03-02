93986ea137bc5263815cf96fba87a36faa2cdf04
KiruthigaManohar <30401289+KiruthigaManohar@users.noreply.github.com>
2018-03-02 14:13:06 +0530

    Create change num.c

---
+++ set 10/change num.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+	int n,a[50],i,j;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		int count=0;
+		for(j=i+1;j<n;j++)
+		{
+		if(a[i]>a[j])
+		{
+			count++;
+		}
+		}
+		if(count!=0)
+		{
+		printf("%d\t",i+1);
+		}
+	
+}
+}
