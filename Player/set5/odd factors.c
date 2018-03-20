17f5979fcf56d0431992f7ef1e92fc297873122f
KiruthigaManohar <30401289+KiruthigaManohar@users.noreply.github.com>
2018-03-20 15:48:56 +0530

    Create odd factors.c

---
+++ Player/set5/odd factors.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int n,i,a,count=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+       a=n%i;
+     if(a==0)
+     if(i%2!=0)
+     {
+     printf("%d\t",i);
+ }
+}
+   
+}
